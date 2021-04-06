b0927eafbf197e88cf80b30de256aa548df63055
ntan1 <ntan1@cmc.com.vn>
2021-04-06 14:54:40 +0700

    handled redirect if user is not logged in

--- src/router/index.js
+++ src/router/index.js
@@ -3,33 +3,47 @@ import Users from '../views/Users.vue';
 import Login from '../views/Login.vue';
 import Posts from '../views/Posts.vue';
 import Register from '../views/Register.vue';
+import store from '../store';
 
 const routes = [
     {
         path: '/',
         name: 'Users',
         component: Users,
+        meta: {
+            requiredAuth: true
+        }
     },
     {
         path: '/posts',
         name: 'Posts',
         component: Posts,
+        meta: {
+            requiredAuth: true
+        }
     },
     {
         path: '/login',
         name: 'Login',
         component: Login,
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register,
-    },
-];
+]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiredAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/login')
+    } else {
+        next()
+    }
+})
 
 export default router;
