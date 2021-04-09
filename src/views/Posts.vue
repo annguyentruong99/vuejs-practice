1877440d7c562dc42b51b911a389b77d156672cb
ntan1 <ntan1@cmc.com.vn>
2021-04-09 15:24:11 +0700

    Finished UI for posts view

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -6,8 +6,55 @@
             </el-col>
         </el-row>
 
-        <el-row>
-            <el-col :span="24"></el-col>
+        <el-row :gutter="24">
+            <el-col v-for="post in posts" :key="post.id" :span="9" :push="3">
+                <el-card shadow="hover" @click="handleClick(post.id)">
+                    <div class="card-content">
+                        <div class="thumbnail">
+                            <img
+                                src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                            />
+                        </div>
+                        <div class="post-text">
+                            <h2>{{ post.title }}</h2>
+                            <p>{{ post.body }}</p>
+                        </div>
+                    </div>
+                </el-card>
+            </el-col>
         </el-row>
     </div>
 </template>
+
+<script>
+    import store from '../store';
+    import router from '../router';
+
+    import { mapState } from 'vuex';
+
+    export default {
+        name: 'Posts',
+        created() {
+            store.dispatch('getPosts');
+        },
+        computed: {
+            ...mapState({
+                posts: (state) => state.posts,
+            }),
+        },
+        methods: {
+            handleClick(id) {
+                router.push(`/post/${id}`);
+            },
+        },
+    };
+</script>
+
+<style>
+    .card-content {
+        display: grid;
+        grid-template-columns: auto auto;
+        grid-column-gap: 10px;
+        overflow: hidden;
+    }
+</style>
